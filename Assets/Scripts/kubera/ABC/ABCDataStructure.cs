2bc958403ade36570e13853a43e5e9bd1504c668
Roberto Langarica <roberto@villavanilla.com>
2015-11-03 20:08:40 -0600

    -El chequeo de carácter por carácter ya jala y ahora solo hay que poblar la sección que valida los comodines

--- Assets/Scripts/kubera/ABC/ABCDataStructure.cs
+++ Assets/Scripts/kubera/ABC/ABCDataStructure.cs
@@ -6,11 +6,16 @@ public class ABCDataStructure : MonoBehaviour {
 	
 	public class IntList
 	{
-		public int value;
-		public bool end;
-		public IntList wildcar;
-		public int wildcarIndex; //Representa el valor que se le da en la busqueda al comodin
-		public List<IntList> content;
+		public int value;//Valor de caracter que representa este nodo
+		public bool end;//Indica si este valor es el final de una palabra
+		public IntList parent = null;//Nodo padre
+		public List<IntList> content;//nodos hijos
+
+		//Para validacion
+		public bool wildcard = false;//Indica si en la validacion se usa como comodin
+		public int wildcardIndex; //Representa el valor que se le da en la busqueda al comodin
+		public int lastCorrectValue;//Ultimo valor correcto en modo comodin
+
 	};
 
 	//Inicio de la estructura
@@ -49,7 +54,7 @@ public class ABCDataStructure : MonoBehaviour {
 
 		int currChar = -1;
 		int limit = chars.Length;
-		List<IntList> currentList = null;
+		IntList currentList = null;
 
 		//Buscamos si ya existe el character
 		IntList tmp = data;
@@ -60,8 +65,8 @@ public class ABCDataStructure : MonoBehaviour {
 			{
 				break;
 			}
-			currentList = tmp.content;
-			tmp = currentList.Find(item => item.value == ABCDataStructure.getCharValue(chars[currChar]));
+			currentList = tmp;
+			tmp = currentList.content.Find(item => item.value == ABCDataStructure.getCharValue(chars[currChar]));
 
 			//Una nueva subpalabra
 			if(tmp != null && currChar == limit-1 && !tmp.end)
@@ -83,8 +88,9 @@ public class ABCDataStructure : MonoBehaviour {
 				tmp.end = currChar == limit-1;
 				//Debug.Log(chars[currChar]+"_"+tmp.value);
 				tmp.content = new List<IntList>();
-				currentList.Add(tmp);
-				currentList = tmp.content;
+				tmp.parent = currentList;
+				currentList.content.Add(tmp);
+				currentList = tmp;
 			}
 
 			wordCount++;
@@ -128,15 +134,18 @@ public class ABCDataStructure : MonoBehaviour {
 	}
 	
 	protected bool isValid;
-	protected List<IntList> currentValidationList;
+	protected IntList currentValidationList;
 	protected List<IntList> levelsOfSearch;
+	protected int levelsCount;
 	public void initCharByCharValidation()
 	{
 		//Valido por default
 		isValid = true;
 		//La raiz de las palabras
-		currentValidationList = data.content;
+		currentValidationList = data;
 
+		//Sin niveles de busqueda
+		levelsCount = 0;
 		if(levelsOfSearch == null)
 		{
 			levelsOfSearch = new List<IntList>();
@@ -154,30 +163,132 @@ public class ABCDataStructure : MonoBehaviour {
 	{
 		IntList tmp = null;
 
+		//Aumentamos el conteo de niveles de busqueda
+		levelsCount++;
+
 		if(isValid)
 		{
 			if(c.wildcard)
 			{
-				//Armamos
-				IntList wildList = new IntList();
+				if(currentValidationList.content.Count > 0)
+				{
+					//Armamos el comodin tomando el primer valor como el correcto
+					IntList wildcard = currentValidationList.content[0];
+					wildcard.wildcardIndex = 0;//Indicamos cual indice tenemos ahorita
+					wildcard.wildcard = true;
+					currentValidationList = wildcard;
 
+					//Correcto sin validacion ya que es comodin
+					return true;
+				}
+				else
+				{
+					//Se estan agregando caracteres de mas
+					//checamos si moviendo comodines
+					return checkBackwardsForWildcardOptions(c);
+				}
 			}
 			else
 			{
-				tmp = currentValidationList.Find(item => item.value == c.value);
+				//Buscamos en el nivel actual si existe el caracter que se pide
+				tmp = currentValidationList.content.Find(item => item.value == c.value);
+
+				if(tmp != null)
+				{
+					//Si existe!!
+					//Guardamos el nuevo nivel de busqueda
+					currentValidationList = tmp;
+					levelsOfSearch.Add(tmp);
+					return true;
+				}
+				else
+				{
+					//No existe!!
+					//checamos si moviendo comodines
+					return checkBackwardsForWildcardOptions(c);
+				}
 			}
 
-			//Guardamos el nuevo nivel de busqueda
-			if(tmp != null)
-			{
-				levelsOfSearch.Add(tmp);
-				return true;
-			}
+
 		}
 
 		//Invalido por default
 		return false;
 	}
+
+	/**
+	 * Elimina un nivel de busqueda, si se elimina un nivel que no es el ultimo
+	 * entonces se eliminaran todos los niveles para no fragmentar la lista,
+	 * la recuperacion de los niveles se debe de manejar afuera donde se controlan los
+	 * caracteres
+	 * */
+	public void deleteLvlOfSearch(int indexToDelete)
+	{
+		//Se elimina un indice mayos a los validados
+		if(indexToDelete >= levelsOfSearch.Count)
+		{
+			//Disminuimos la cuenta de los niveles
+			levelsCount--;
+
+			//La lista es valida de nuevo?
+			if(levelsCount <= levelsOfSearch.Count)
+			{
+				isValid = true;
+			}
+		}
+		else 
+		{
+			//Eliminamos todos los niveles sobrantes
+			for(int i = levelsOfSearch.Count-1; i >= indexToDelete; i--)
+			{
+				//Disminuimos la cuenta de los niveles
+				levelsCount--;
+				//Quitamos cualquier comodin
+				levelsOfSearch[i].wildcard = false;
+				levelsOfSearch.RemoveAt(i);
+			}
+
+			//Hacemos reset de currentValidationList para continuar la busqueda de la manera correcta
+			if(levelsCount > 0)
+			{
+				//Si el nivel que quedo es comodin lo reseteamos al estado default
+				if(levelsOfSearch[levelsCount-1].wildcard)
+				{
+					levelsOfSearch[levelsCount-1].wildcard = false;
+
+					//Armamos el comodin tomando el primer valor como el correcto
+					IntList wildcard = levelsOfSearch[levelsCount-1].parent.content[0];
+					wildcard.wildcardIndex = 0;//Indicamos cual indice tenemos ahorita
+					wildcard.wildcard = true;
+					levelsOfSearch[levelsCount-1] = wildcard;
+				}
+
+				currentValidationList = levelsOfSearch[levelsCount-1];
+			}
+			else
+			{
+				//Queda como en estado inicial
+				initCharByCharValidation();
+			}
+		}
+
+	}
+
+	/**
+	 * Hace un chequeo de los niveles de busqueda hacia atras exclusivamente
+	 * para mover los comodines hacia un estado donde se admita charToValidate.
+	 * 
+	 * Si no existe ningun estado valido entonces devuelve todos los comodines al ultimo estado
+	 * activo.
+	 * 
+	 * 
+	 * @return true si el caracter es valido. false si el caracter es invalido
+	 * */
+	protected bool checkBackwardsForWildcardOptions(ABCChar charToValidate)
+	{
+		return false;
+	}
+
 
 	protected bool hasValueInChildren(IntList List, int searchValue)
 	{
