76a28823e636ad41132d6c867cd78fdf5fdc628c
AliVilla <ali@villavanilla.com>
2015-11-03 18:30:08 -0600

    Tablero y analizador

---
+++ Assets/Scripts/kubera/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour 
+{
+	[HideInInspector]
+	public Levels data;
+
+	// Use this for initialization
+	void Start () 
+	{
+		TextAsset tempTxt = (TextAsset)Resources.Load ("levels");
+		data = Levels.LoadFromText(tempTxt.text);
+
+		Debug.Log(data.levels[0].pool);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
