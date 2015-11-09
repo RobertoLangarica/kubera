55f3019a553e9a5a0abdebb3dfac3b6710cd05f5
AliVilla <ali@villavanilla.com>
2015-11-09 13:06:42 -0600

    Correccion de errores

--- Assets/Scripts/kubera/Pieces/Piece.cs
+++ Assets/Scripts/kubera/Pieces/Piece.cs
@@ -26,6 +26,7 @@ public class Piece : MonoBehaviour {
 	
 	protected ECOLORS_ID currentColor;
 	protected Color rendererColor;
+	protected float currentAlpha;
 
 	[HideInInspector]
 	public Transform myFirstPos;
@@ -42,11 +43,12 @@ public class Piece : MonoBehaviour {
 
 	protected void selectAColor()
 	{
-		Color myColor = new Color(Random.Range(0.0f,1.0f),Random.Range(0.0f,1.0f),Random.Range(0.0f,1.0f));
+		//Color myColor = new Color(Random.Range(0.0f,1.0f),Random.Range(0.0f,1.0f),Random.Range(0.0f,1.0f));
+		color = ECOLORS_ID(Random.Range(1,9));
 
 		foreach(GameObject piece in pieces)
 		{
-			piece.GetComponent<SpriteRenderer>().color = myColor;
+			piece.GetComponent<SpriteRenderer>().color = rendererColor;
 		}
 	}
 
@@ -63,34 +65,34 @@ public class Piece : MonoBehaviour {
 			
 			switch(currentColor)
 			{
-			case EShapeColor.AQUA:
+			case ECOLORS_ID.AQUA:
 				rendererColor = new Color(0.376f, 0.698f, 0.639f); 
 				break;
-			case EShapeColor.BLACK:
+			case ECOLORS_ID.BLACK:
 				rendererColor = new Color(0.180f, 0.188f, 0.192f);
 				break;
-			case EShapeColor.BLUE:
+			case ECOLORS_ID.BLUE:
 				rendererColor = new Color(0.133f, 0.565f, 0.945f);
 				break;
-			case EShapeColor.GREEN:
+			case ECOLORS_ID.GREEN:
 				rendererColor = new Color(0.192f, 0.545f, 0.263f);
 				break;
-			case EShapeColor.GREY:
+			case ECOLORS_ID.GREY:
 				rendererColor = new Color(0.392f, 0.514f, 0.584f);
 				break;
-			case EShapeColor.MAGENTA:
+			case ECOLORS_ID.MAGENTA:
 				rendererColor = new Color(0.643f, 0.059f, 0.482f);
 				break;
-			case EShapeColor.RED:
+			case ECOLORS_ID.RED:
 				rendererColor = new Color(0.965f, 0.282f, 0.427f);
 				break;
-			case EShapeColor.WHITE:
+			case ECOLORS_ID.WHITE:
 				rendererColor = new Color(0.910f, 0.937f, 0.957f);
 				break;
-			case EShapeColor.YELLOW:
+			case ECOLORS_ID.YELLOW:
 				rendererColor = new Color(0.976f, 0.627f, 0.000f);
 				break;
-			case EShapeColor.NONE:
+			case ECOLORS_ID.NONE:
 				return;
 			}
 			
