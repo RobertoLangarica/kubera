229aa5e8ed65f272ba9b73712ad367e0ac7f2566
Ricardo Alvarez Gonzalez <ricardo@villavanilla.com>
2015-11-11 18:36:30 -0600

    show nextButton

--- Assets/Scripts/kubera/Input/InputGameController.cs
+++ Assets/Scripts/kubera/Input/InputGameController.cs
@@ -187,6 +187,7 @@ public class InputGameController : MonoBehaviour {
 				if(gesture.Raycast.Hit2D.transform.gameObject.GetComponent<Tile>())
 				{
 					gesture.Raycast.Hit2D.transform.gameObject.GetComponent<Tile>().ShootLetter();
+					FindObjectOfType<ShowNext>().ShowingNext(true);
 				}
 			}
 		}
