e07056073f2aab3b77cb445ba8dadce0740d277d
Ali <ali@villavanilla.com>
2016-03-14 17:43:19 -0600

    Subo la primera prueba de los textos por idioma

---
+++ Assets/Scripts/kubera/HUD/TextXML.cs
@@ -0,0 +1,18 @@
+﻿using System.Xml;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+
+public class TextXML
+{
+	protected List<LanguageXML> _languages = new List<LanguageXML>();
+
+	[XmlAttribute("id")]
+	public string id;
+	
+	[XmlArray("languages"),XmlArrayItem("language")]
+	public LanguageXML[] languages
+	{
+		set{_languages = new List<LanguageXML>(value);}
+		get{return _languages.ToArray();}
+	}
+}
