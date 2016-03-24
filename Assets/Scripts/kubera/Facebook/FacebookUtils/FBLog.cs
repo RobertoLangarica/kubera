df9a145ac2e76b2b8796166de2fb40a9181e5535
Ricardo Alvarez Gonzalez <ricardo@villavanilla.com>
2016-03-23 19:03:12 -0600

    contenido de facebook scripts para que funcione los mensajes y los request en escena map funciona el boton de facebook y hace chequeo si esta conectado para saber que mostrar de ser necesario

---
+++ Assets/Scripts/kubera/Facebook/FacebookUtils/FBLog.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+using Facebook.Unity;
+
+public class FBLog : MonoBehaviour {
+
+	public delegate void DOnLoginComplete(bool complete);
+
+	public DOnLoginComplete onLoginComplete;
+	void Awake()
+	{
+		// Initialize FB SDK
+		if (!FB.IsInitialized)
+		{
+			FB.Init(InitCallback);
+		}
+	}
+
+	public void InitCallback()
+	{
+		Debug.Log("InitCallback");
+
+		// App Launch events should be logged on app launch & app resume
+		// See more: https://developers.facebook.com/docs/app-events/unity#quickstart
+		FB.ActivateApp();
+
+		if (FB.IsLoggedIn) 
+		{
+			Debug.Log("Already logged in");
+			OnLoginComplete();
+		}
+		else
+		{
+			onLoginComplete (false);
+		}
+	}
+
+	private void OnLoginComplete()
+	{
+		Debug.Log("OnLoginComplete");
+
+		if (!FB.IsLoggedIn)
+		{
+			//TODO: hacer el boton de login activo para que el usuario intente conectarse
+			onLoginComplete (false);
+			//facebookBtn.SetActive (true);
+		}
+		else
+		{
+			onLoginComplete (true);
+		}
+	}
+
+	public void OnLoginClick ()
+	{
+		Debug.Log("OnLoginClick");
+
+		// hacer inactivo el boton para hacer login
+		//facebookBtn.SetActive (false);
+
+		// Call Facebook Login for Read permissions of 'public_profile', 'user_friends', and 'email'
+		FBPermissions.PromptForLogin(OnLoginComplete);
+		FBPermissions.PromptForPublish(OnLoginComplete);
+	}
+
+	public void LogOut ()
+	{
+		FB.LogOut ();
+	}
+
+	void OnApplicationPause (bool pauseStatus)
+	{
+		#if UNITY_EDITOR
+		return;
+		#endif
+		#if UNITY_ANDROID || UNITY_IOS
+		// Check the pauseStatus to see if we are in the foreground
+		// or background
+		if (!pauseStatus) {
+			//app resume
+			if (FB.IsInitialized) 
+			{
+				FB.ActivateApp();
+			} 
+			else 
+			{
+				//Handle FB.Init
+				FB.Init( () => {
+					FB.ActivateApp();
+				});
+			}
+		}	
+		#endif
+	}
+}
