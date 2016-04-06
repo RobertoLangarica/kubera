a4120a4a3f23f6cbbe343cdfc81c6af91088d9cf
Ricardo Alvarez Gonzalez <ricardo@villavanilla.com>
2016-04-06 13:31:40 -0500

    wordmanager con  el cambio de boton de borrar y aceptar palabra

--- Assets/Scripts/kubera/ABC/WordManager.cs
+++ Assets/Scripts/kubera/ABC/WordManager.cs
@@ -26,6 +26,7 @@ public class WordManager : MonoBehaviour
 
 
 	public GameObject wordCompleteButton;
+	public GameObject wordDeleteButton;
 	public Image deleteButtonImage;
 	public Sprite deleteCharacterState;
 	public Sprite deleteWordState;
@@ -183,7 +184,7 @@ public class WordManager : MonoBehaviour
 		//Los comodines no se pueden destruir
 		if(!letter.abcChar.wildcard && isOverDeleteArea(letter.transform.localPosition))
 		{
-			removeLetter(letter);
+			//removeLetter(letter);
 		}
 		else
 		{
@@ -368,9 +369,12 @@ public class WordManager : MonoBehaviour
 
 	private void afterWordValidation()
 	{
-		activateWordCompleteBtn(wordsValidator.isCompleteWord());
+		bool completeWord = wordsValidator.isCompleteWord ();
 
-		if(wordsValidator.isCompleteWord())
+		activateWordCompleteBtn(completeWord);
+		activateWordDeleteBtn (!completeWord);
+
+		if(completeWord)
 		{
 			Debug.Log("Se completo: "+getCurrentWordOnList());
 		}
@@ -381,12 +385,17 @@ public class WordManager : MonoBehaviour
 		wordCompleteButton.SetActive (active);
 	}
 
+	public void activateWordDeleteBtn(bool active)
+	{
+		wordDeleteButton.SetActive (active);
+	}
+
 	private void onLettersChange()
 	{
 		updateWordPoints();
 
-		afterWordValidation();
-		activateWordDeleteBtn(isThereAnyLetterOnContainer());
+		//afterWordValidation();
+		//activateWordDeleteBtn(isThereAnyLetterOnContainer());
 		changeDeleteState(EDeleteState.WORD);
 
 		onWordChange ();
@@ -442,11 +451,6 @@ public class WordManager : MonoBehaviour
 		return result;
 	}
 
-	public void activateWordDeleteBtn(bool active)
-	{
-		deleteButtonImage.gameObject.SetActive(active);
-	}
-
 	private bool isThereAnyLetterOnContainer()
 	{
 		return (letterContainer.transform.childCount == 0 ? false:true); 
@@ -460,7 +464,7 @@ public class WordManager : MonoBehaviour
 			deleteButtonImage.sprite = deleteWordState;
 			break;
 		case EDeleteState.CHARACTER:
-			deleteButtonImage.sprite = deleteCharacterState;
+			//deleteButtonImage.sprite = deleteCharacterState;
 			break;
 		}
 	}
