bb5e04ee2ec5bf7bfcfcc9cacaa5780c12a9d359
Roberto <roberto@villavanilla.com>
2016-06-02 18:07:33 -0500

    -Parse stuff

---
+++ Assets/Scripts/Data/Remote/ParseManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using Parse;
+
+public class ParseManager : MonoBehaviour 
+{
+	void Start()
+	{
+		ParseObject testObject = new ParseObject("TestObject");
+		testObject["foo"] = "REMOTE TEst";
+		testObject.SaveAsync();
+	}
+}
