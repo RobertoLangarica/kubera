b010f68de7b88cf5f3c238ccbdf88748839649e0
Ricardo Alvarez Gonzalez <ricardo@villavanilla.com>
2016-09-08 11:33:13 -0500

    updateTexturas

--- Assets/Scripts/kubera/Map/MiniWorld.cs
+++ Assets/Scripts/kubera/Map/MiniWorld.cs
@@ -1,6 +1,8 @@
-﻿using UnityEngine;
+﻿using DG.Tweening;
+using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
 public class MiniWorld : MonoBehaviour {
 
@@ -15,6 +17,8 @@ public class MiniWorld : MonoBehaviour {
 	public bool bloqued;
 
 	[HideInInspector]public WorldsPopUp worldPopUp;
+
+	public Transform[] lights;
 
 	void Start()
 	{
@@ -38,5 +42,28 @@ public class MiniWorld : MonoBehaviour {
 		obtained.SetActive (false);
 		imageBloqued.SetActive (true);
 		starImage.enabled = false;
+	}
+
+	public void animateLights()
+	{
+		float randomRotation = Random.Range (180f, 360f);
+		for(int i=0; i<lights.Length; i++)
+		{
+			lights [i].DORotate (new Vector3 (0, 0, lights [i].rotation.z * randomRotation), Random.Range (1, 3), RotateMode.FastBeyond360).OnComplete(()=>
+				{
+					if(i+1 == lights.Length)
+					{
+						Invoke("animateLights",Random.Range(3,5));
+					}
+				}).SetId(lights[i]);
+		}
+	}
+
+	public void killAnimateLights()
+	{
+		for(int i=0; i<lights.Length; i++)
+		{
+			DOTween.Kill (lights [i], true);
+		}
 	}
 }
