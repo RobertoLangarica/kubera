518ca6373f9a9e997e37f870934f5edca4528114
Ali <ali@villavanilla.com>
2016-10-13 17:58:44 -0500

    NUEVO DICCIONARIO, popup para entrar a shopika, bandera para bloquear analitycs

--- Assets/Scripts/kubera/Game/GameManager.cs
+++ Assets/Scripts/kubera/Game/GameManager.cs
@@ -161,7 +161,8 @@ public class GameManager : MonoBehaviour
 		}
 		else
 		{
-			configureLevel(PersistentData.GetInstance().currentLevel);
+			configureLevel(PersistentData.GetInstance().getLevelByIndex(162));
+			//configureLevel(PersistentData.GetInstance().currentLevel);
 		}
 
 		if(AudioManager.GetInstance())
