8b9d63308cb530635a78aa5a2ba7c4b0d06a1023
Roberto <roberto@villavanilla.com>
2016-10-13 18:02:27 -0500

    -Tips implementados para las multiples líneas

---
+++ Assets/Scripts/utils/gems/remote/SPRequest/SPRegisterInvite.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using Data.Remote;
+
+using BestHTTP;
+
+using utils.gems.remote.response;
+
+namespace utils.gems.remote.request
+{
+	public class SPRegisterInvite : RemoteRequest<SPGetGemsResponse>  
+	{
+		public string playerId;
+		public string acces_token;
+		public string invitedEmail;
+		public string invitedFacebookId;
+		public string invitedId;
+		public string invitedPhoneNumber;
+		public string inviterFacebookId;
+		public string inviterId;
+
+		public override string getCompletePath (string path)
+		{
+			return path+"/Invites?access_token="+acces_token;
+		}
+
+	}
+}
