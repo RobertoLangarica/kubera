9892e8b308f4bd170f38ac064647463f43bdeba5
Ricardo Alvarez Gonzalez <ricardo@villavanilla.com>
2016-10-18 14:09:38 -0500

    quito mascara de los puntos y estrellas

--- Assets/Scripts/kubera/HUD/HUDMetterAndStars.cs
+++ Assets/Scripts/kubera/HUD/HUDMetterAndStars.cs
@@ -4,6 +4,9 @@ using UnityEngine.UI;
 public class HUDMetterAndStars : MonoBehaviour
 {
 	public Image pointsMeter;
+	public Image[] StarsFilled;
+	public Image[] BallsFilled;
+	public Image[] Balls;
 	public Image[] Stars;
 	public Sprite StarFilled;
 
@@ -28,13 +31,15 @@ public class HUDMetterAndStars : MonoBehaviour
 	 **/
 	public void setMeterPoints(int points)
 	{
-		float size = (float)points / scoreToReachStar [2];
+		/*float size = (float)points / scoreToReachStar [2];
 		if (size >= 1)
 		{
 			size = 1;
 		}
-		//pointsMeter.rectTransform.anchorMax = new Vector3 (size,1,0);
+		//pointsMeter.rectTransform.anchorMax = new Vector3 (size,1,0);*/
+
 		actualizeStars (points);
+		actualizeBalls (points);
 	}
 
 	/**
@@ -52,9 +57,9 @@ public class HUDMetterAndStars : MonoBehaviour
 	public void setStarsPosition()
 	{
 		float pointMetterwidth = pointsMeter.rectTransform.rect.width;
-		Stars[0].rectTransform.anchoredPosition = new Vector3(scoreToReachStar[0] / scoreToReachStar [2] * pointMetterwidth, 0);
-		Stars[1].rectTransform.anchoredPosition = new Vector3(scoreToReachStar[1] / scoreToReachStar [2] * pointMetterwidth, 0);
-		Stars[2].rectTransform.anchoredPosition =  new Vector2(pointMetterwidth,0);
+		StarsFilled[0].rectTransform.anchoredPosition = new Vector3(scoreToReachStar[0] / scoreToReachStar [2] * pointMetterwidth, 0);
+		StarsFilled[1].rectTransform.anchoredPosition = new Vector3(scoreToReachStar[1] / scoreToReachStar [2] * pointMetterwidth, 0);
+		StarsFilled[2].rectTransform.anchoredPosition =  new Vector2(pointMetterwidth,0);
 	}
 
 	/**
@@ -79,7 +84,11 @@ public class HUDMetterAndStars : MonoBehaviour
 			if (!star3Reached && size >= Star3) 
 			{
 				size = 1;
-				Stars [2].transform.localScale *= 1.1f;
+
+				StarsFilled [2].gameObject.SetActive (true);
+				Stars [2].gameObject.SetActive (false);
+
+				StarsFilled [2].transform.localScale *= 1.1f;
 				star3Reached = true;
 			}
 		}
@@ -95,7 +104,10 @@ public class HUDMetterAndStars : MonoBehaviour
 
 			if(size >= Star2)
 			{
-				Stars [1].transform.localScale *= 1.1f;
+				StarsFilled [1].gameObject.SetActive (true);
+				Stars [1].gameObject.SetActive (false);
+
+				StarsFilled [1].transform.localScale *= 1.1f;
 				//Stars [1].sprite = StarFilled;
 				star2Reached = true;
 			}
@@ -109,13 +121,55 @@ public class HUDMetterAndStars : MonoBehaviour
 
 			if(size >= Star1)
 			{
-				Stars [0].transform.localScale *= 1.1f;
+				StarsFilled [0].gameObject.SetActive (true);
+				Stars [0].gameObject.SetActive (false);
+
+				StarsFilled [0].transform.localScale *= 1.1f;
 				star1Reached = true;
 			}
 		}
 
 
 		pointsMeter.fillAmount = size;
+	}
+
+	protected void actualizeBalls(int points)
+	{
+		if(!BallsFilled[0].gameObject.activeSelf)
+		{
+			if(pointsMeter.fillAmount >= 0.106f)
+			{
+				BallsFilled [0].gameObject.SetActive (true);
+				Balls[0].gameObject.SetActive (false);
+			}
+		}
+
+		if(!BallsFilled[1].gameObject.activeSelf)
+		{
+			if(pointsMeter.fillAmount >= 0.3775f)
+			{
+				BallsFilled [1].gameObject.SetActive (true);
+				Balls[1].gameObject.SetActive (false);
+			}
+		}
+
+		if(!BallsFilled[2].gameObject.activeSelf)
+		{
+			if(pointsMeter.fillAmount >= 0.662f)
+			{
+				BallsFilled [2].gameObject.SetActive (true);
+				Balls[2].gameObject.SetActive (false);
+			}
+		}
+
+		if(!BallsFilled[3].gameObject.activeSelf)
+		{
+			if(pointsMeter.fillAmount >= 0.7705f)
+			{
+				BallsFilled [3].gameObject.SetActive (true);
+				Balls[3].gameObject.SetActive (false);
+			}
+		}
 	}
 
 	public int getStarsAmount()
